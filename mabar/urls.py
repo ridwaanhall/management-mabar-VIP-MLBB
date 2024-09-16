a48cf8021b3f88f3eb96378a4e076d3ddcab8d7b
ridwaanhall <erbyl14@gmail.com>
2024-09-16 23:49:04 +0700

    update url for public access

--- mabar/urls.py
+++ mabar/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('antri-mabar/', views.mabar_list, name='mabar_list'),
-    path('bonus-skin/', views.bonus_skin_list, name='bonus_skin_list'),
-    path('request-hero/', views.request_hero_list, name='request_hero_list'),
+    path('list-mabar/', views.mabar_list, name='mabar_list'),
+    path('list-free-skin/', views.bonus_skin_list, name='bonus_skin_list'),
+    path('list-req-hero/', views.request_hero_list, name='request_hero_list'),
 ]

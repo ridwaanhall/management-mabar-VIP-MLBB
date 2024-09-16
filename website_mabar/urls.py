ac608438c10c34ce1da2b7864441c4f6792f82d4
ridwaanhall <erbyl14@gmail.com>
2024-09-16 22:58:41 +0700

    add public access

--- website_mabar/urls.py
+++ website_mabar/urls.py
@@ -5,4 +5,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', lambda request: redirect('admin/mabar/mabar/')),
+    path('public/', include('mabar.urls')),
 ]

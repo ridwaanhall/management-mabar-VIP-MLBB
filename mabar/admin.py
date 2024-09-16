9a54b528b36c417da2aa34d989b9ab91373f89a2
ridwaanhall <erbyl14@gmail.com>
2024-09-16 19:09:42 +0700

    create table for mabar record, request hero, and bonus skin

---
+++ mabar/admin.py
@@ -0,0 +1,183 @@
+import datetime
+import csv
+from io import BytesIO
+import openpyxl
+from django.contrib import admin
+from django.utils.html import format_html
+from django.http import HttpResponse
+from .models import Mabar, BonusSkin, RequestHero
+from django.utils.timezone import localtime
+
+class DateRangeFilter(admin.SimpleListFilter):
+    title = 'Date Created'
+    parameter_name = 'date_created_range'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('last_7_days', 'Last 7 days'),
+            ('last_30_days', 'Last 30 days'),
+            ('this_month', 'This month'),
+            ('last_month', 'Last month'),
+        )
+
+    def queryset(self, request, queryset):
+        from datetime import timedelta
+        from django.utils import timezone
+
+        now = timezone.now()
+        if self.value() == 'last_7_days':
+            start_date = now - timedelta(days=7)
+        elif self.value() == 'last_30_days':
+            start_date = now - timedelta(days=30)
+        elif self.value() == 'this_month':
+            start_date = now.replace(day=1)
+        elif self.value() == 'last_month':
+            last_month = now.month - 1 if now.month > 1 else 12
+            year = now.year if now.month > 1 else now.year - 1
+            start_date = timezone.make_aware(datetime.date(year, last_month, 1))
+            end_date = timezone.make_aware(datetime.date(year, last_month + 1, 1))
+            return queryset.filter(date_created__range=(start_date, end_date))
+        else:
+            return queryset
+
+        return queryset.filter(date_created__gte=start_date)
+
+@admin.register(Mabar)
+class MabarAdmin(admin.ModelAdmin):
+    list_display = ['date_created', 'donate_name', 'jumlah_game', 'telah_digunakan', 'nickname', 'id_user', 'zone_user', 'colored_status', 'catatan', 'is_vvip', 'is_done']
+    list_filter = ['status', 'is_vvip', 'is_done', 'date_created', DateRangeFilter]
+    search_fields = ['donate_name', 'id_user']
+    readonly_fields = ['date_created']
+    ordering = ['date_created']
+    list_per_page = 25  # Pagination: Display 25 records per page
+    actions = ['mark_as_salah_id', 'mark_as_kurang', 'mark_as_prepare', 'mark_as_in_mabar', 
+               'mark_as_done', 'export_as_csv', 'export_as_excel']
+
+    def colored_status(self, obj):
+        color = {
+            'antri': 'orange',
+            'prepare': 'blue',
+            'in_mabar': 'lightgreen',
+            'salah_id': 'red',
+            'kurang': 'red',
+            'done': 'green',
+        }.get(obj.status, 'black')
+        return format_html(f'<span style="color: {color};">{obj.get_status_display()}</span>')
+    
+    colored_status.short_description = 'Status'
+
+    fieldsets = (
+        ('Data Donasi', {
+            'fields': ('donate_name', 'jumlah_game', 'telah_digunakan')
+        }),
+        ('Data Player', {
+            'fields': ('nickname', 'id_user', 'zone_user')
+        }),
+        ('Status dan Tanggal', {
+            'fields': ('status', 'catatan', 'is_vvip', 'is_done', 'date_created'),
+        }),
+    )
+
+    # Actions
+    def mark_as_salah_id(self, request, queryset):
+        updated = queryset.update(status='salah_id', catatan='Pastikan ID kamu sudah benar!', is_done=False)
+        self.message_user(request, f'{updated} mabar record(s) marked as Salah ID User.')
+    mark_as_salah_id.short_description = 'Mark selected records as Salah ID User'
+
+    def mark_as_kurang(self, request, queryset):
+        updated = queryset.update(status='kurang', catatan='Whoopp! Dana Mabarmu kurang nih!', is_done=False)
+        self.message_user(request, f'{updated} mabar record(s) marked as Donasi Kurang.')
+    mark_as_kurang.short_description = 'Mark selected records as Donasi Kurang'
+
+    def mark_as_prepare(self, request, queryset):
+        updated = queryset.update(status='prepare', catatan='Harap untuk login dan bersiap untuk mabar!', is_done=False)
+        self.message_user(request, f'{updated} mabar record(s) marked as Persiapan.')
+    mark_as_prepare.short_description = 'Mark selected records as Persiapan'
+    
+    def mark_as_in_mabar(self, request, queryset):
+        updated_count = 0
+        for obj in queryset:
+            # Increase the 'telah_digunakan' field value by 1
+            obj.telah_digunakan = (obj.telah_digunakan or 0) + 1
+            
+            # Check if 'telah_digunakan' equals 'jumlah_game'
+            if obj.telah_digunakan >= obj.jumlah_game:
+                obj.status = 'done'
+                obj.catatan='Mabar Selesai. Terima Kasih :)'
+                obj.is_done = True  # Mark as done if 'telah_digunakan' equals or exceeds 'jumlah_game'
+            else:
+                obj.status = 'in_mabar'
+                obj.catatan=f'Telah digunakan {obj.telah_digunakan} dari {obj.jumlah_game} game'
+                obj.is_done = False
+
+            obj.save()  # Save the updated object
+            updated_count += 1
+
+        self.message_user(request, f'{updated_count} mabar record(s) marked as Sedang Mabar or Done as applicable.')
+    mark_as_in_mabar.short_description = 'Mark selected records as Sedang Mabar or Done if "telah_digunakan" equals "jumlah_game"'
+
+    def mark_as_done(self, request, queryset):
+        updated = queryset.update(status='done', catatan='Mabar Selesai. Terima Kasih :)', is_done=True)
+        self.message_user(request, f'{updated} mabar record(s) marked as done.')
+    mark_as_done.short_description = 'Mark selected records as Done'
+    
+    def export_as_csv(self, request, queryset):
+        meta = self.model._meta
+        field_names = [field.name for field in meta.fields]
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = f'attachment; filename={meta}.csv'
+        writer = csv.writer(response)
+        writer.writerow(field_names)
+        for obj in queryset:
+            writer.writerow([getattr(obj, field) for field in field_names])
+        return response
+
+    export_as_csv.short_description = "Export Selected as CSV"
+
+    def export_as_excel(self, request, queryset):
+        meta = self.model._meta
+        field_names = [field.name for field in meta.fields]
+        
+        # Create a workbook and a worksheet
+        wb = openpyxl.Workbook()
+        ws = wb.active
+        ws.title = meta.verbose_name_plural
+
+        # Write the header row
+        ws.append(field_names)
+
+        for obj in queryset:
+            row = []
+            for field in field_names:
+                value = getattr(obj, field)
+                if isinstance(value, datetime.datetime):
+                    # Convert timezone-aware datetime to naive datetime
+                    if value.tzinfo is not None:
+                        value = localtime(value).replace(tzinfo=None)
+                row.append(value)
+            ws.append(row)
+        
+        # Save the workbook to a BytesIO object
+        output = BytesIO()
+        wb.save(output)
+        output.seek(0)
+        
+        # Create an HTTP response with the Excel file
+        response = HttpResponse(output, content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+        response['Content-Disposition'] = f'attachment; filename={meta.verbose_name_plural}.xlsx'
+        
+        return response
+
+    export_as_excel.short_description = "Export Selected as Excel"
+
+@admin.register(BonusSkin)
+class BonusSkinAdmin(admin.ModelAdmin):
+    list_display = ['date_created', 'mabar', 'hero_digunakan', 'skin_request', 'terkirim', 'date_terkirim']
+    list_filter = ['terkirim']
+    search_fields = ['mabar__id_user', 'hero_digunakan', 'skin_request']
+    
+@admin.register(RequestHero)
+class RequestHeroAdmin(admin.ModelAdmin):
+    list_display = ('date_created', 'donate_name', 'type_request', 'hero_name', 'type_lane', 'count', 'is_done', 'catatan')
+    list_filter = ('type_request', 'is_done')
+    search_fields = ('donate_name', 'hero_name')
---
+++ mabar/models.py
@@ -0,0 +1,156 @@
+from django.db import models
+from django.utils import timezone
+
+class Mabar(models.Model):
+    STATUS_CHOICES = [
+        ('antri', 'Dalam Antrian'),
+        ('salah_id', 'Salah ID User'),
+        ('kurang', 'Donasi Kurang'),
+        ('prepare', 'Persiapan'),
+        ('in_mabar', 'Sedang Mabar'),
+        ('done', 'Selesai'),
+    ]
+
+    donate_name = models.CharField(
+        max_length=100, 
+        verbose_name="Pendonasi"
+    )
+    nickname = models.CharField(
+        max_length=100, 
+        blank=True, 
+        null=True, 
+        verbose_name="Nickname"
+    )
+    id_user = models.IntegerField(
+        verbose_name="ID"
+    )
+    zone_user = models.IntegerField(
+        blank=True, 
+        null=True, 
+        verbose_name="Zone"
+    )
+    jumlah_game = models.IntegerField(
+        default=1, 
+        verbose_name="Jumlah"
+    )
+    telah_digunakan = models.IntegerField(
+        default=0, 
+        verbose_name="Digunakan"
+    )
+    catatan = models.TextField(
+        default="Dalam antrian... Harap selalu pantau update status dan catatan, yaa!!",
+        blank=True, 
+        null=True, 
+        verbose_name="Catatan"
+    )
+    status = models.CharField(
+        max_length=10, 
+        choices=STATUS_CHOICES, 
+        default='antri', 
+        verbose_name="Status"
+    )
+    date_created = models.DateTimeField(
+        auto_now_add=True, 
+        verbose_name="Tanggal Dibuat"
+    )
+    is_vvip = models.BooleanField(
+        default=False, 
+        verbose_name="Is VVIP"
+    )
+    is_done = models.BooleanField(
+        default=False, 
+        verbose_name="Is Done"
+    )
+
+    class Meta:
+        verbose_name = 'Mabar Record'
+        verbose_name_plural = 'Mabar Records'
+        ordering = ['-date_created']
+
+    def __str__(self):
+        return f"{self.donate_name} - {self.id_user} {self.nickname}"
+
+class BonusSkin(models.Model):
+    mabar = models.ForeignKey(Mabar, on_delete=models.CASCADE, verbose_name="ID User")  # ForeignKey relationship
+    hero_digunakan = models.CharField(max_length=100, verbose_name="Hero Digunakan")
+    skin_request = models.CharField(max_length=100, verbose_name="Skin Request")
+    terkirim = models.BooleanField(default=False, verbose_name="Terkirim")
+    date_created = models.DateTimeField(auto_now_add=True, verbose_name="Tanggal Dibuat")
+    date_terkirim = models.DateTimeField(null=True, blank=True, verbose_name="Tanggal Terkirim")
+
+    class Meta:
+        verbose_name = 'Bonus Skin'
+        verbose_name_plural = 'Bonus Skins'
+
+    def save(self, *args, **kwargs):
+        # Check if terkirim is set to True and date_terkirim is empty
+        if self.terkirim and self.date_terkirim is None:
+            self.date_terkirim = timezone.now()  # Set current time to date_terkirim
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"Bonus Skin for {self.mabar.id_user} - {self.hero_digunakan} ({'Terkirim' if self.terkirim else 'Belum Terkirim'})"
+    
+class RequestHero(models.Model):
+    TYPE_REQUEST_CHOICES = [
+        ('meta', 'Hero Meta'),
+        ('non_meta', 'Hero Non-Meta'),
+        ('troll', 'Hero Troll'),
+        ('lain', 'Lain-lain')
+    ]
+    
+    TYPE_LANE_CHOICES = [
+        ('roam', 'Roam'),
+        ('gold', 'Gold Lane'),
+        ('jungle', 'Jungle'),
+        ('mid', 'Mid Lane'),
+        ('exp', 'EXP Lane'),
+        ('bebas', 'Bebas'),
+    ]
+
+    donate_name = models.CharField(
+        max_length=100,
+        verbose_name="Pendonasi"
+    )
+    type_request = models.CharField(
+        max_length=10,
+        choices=TYPE_REQUEST_CHOICES,
+        default='lain',
+        verbose_name="Tipe Request"
+    )
+    hero_name = models.CharField(
+        max_length=100,
+        verbose_name="Nama Hero"
+    )
+    type_lane = models.CharField(
+        max_length=10,
+        choices=TYPE_LANE_CHOICES,
+        default='bebas',
+        verbose_name="Tipe Lane"
+    )
+    count = models.IntegerField(
+        default=1,
+        verbose_name="Jumlah Request"
+    )
+    catatan = models.TextField(
+        default="-",
+        blank=True,
+        verbose_name="Catatan"
+    )
+    is_done = models.BooleanField(
+        default=False,
+        verbose_name="Sudah Selesai"
+    )
+
+    date_created = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="Tanggal Dibuat"
+    )
+
+    class Meta:
+        verbose_name = 'Request Hero'
+        verbose_name_plural = 'Request Heroes'
+        ordering = ['-date_created']
+
+    def __str__(self):
+        return f"{self.donate_name} - {self.hero_name} ({self.count} request)"

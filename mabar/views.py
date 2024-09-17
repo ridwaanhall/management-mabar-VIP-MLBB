822d0047bb31ba17dc10c3ca89d71dd50063a4cf
ridwaanhall <erbyl14@gmail.com>
2024-09-17 14:36:29 +0700

    update records filters

--- mabar/views.py
+++ mabar/views.py
@@ -4,11 +4,15 @@ from .models import Mabar, BonusSkin, RequestHero
 # View untuk menampilkan daftar Mabar Record
 def mabar_list(request):
     records = Mabar.objects.all().order_by('date_created')
+    records_show_all_except_done = records.filter(is_done=False)
     records_filter_by_status_prepare = records.filter(status='prepare', is_done=False)
+    records_show_all_done = records.filter(is_done=True)
     
     context = {
-        'records': records,
-        'prepare_records': records_filter_by_status_prepare
+        'records_all': records,
+        'records_except_done': records_show_all_except_done,
+        'prepare_records': records_filter_by_status_prepare,
+        'done_records': records_show_all_done
     }
     return render(request, 'public/mabar_list.html', context)
 
